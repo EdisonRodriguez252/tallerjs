499bfe48dc0044594e1f3119ce80f2b68896eddb
EdisonRodriguez252 <junilozano1@gmail.com>
2023-04-07 13:32:38 -0500

    1.0

---
+++ js/fl.js
@@ -0,0 +1,22 @@
+console.log("hola");
+
+var a = 4;
+var b = 3;
+
+if(a<b){
+    console.log(a);
+}else if(b<a){
+    console.log(b);
+}
+
+var c = 24
+var d = 14
+var e = 3
+
+if(c>d && c>e){
+    console.log(c);
+}else if(d>e && d>c){
+    console.log(d);
+}else if(e>d && e>c){
+    console.log(e);
+}
